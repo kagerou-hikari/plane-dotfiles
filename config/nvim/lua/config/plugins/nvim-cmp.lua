6e1c841bbf3d24448b2ad7a87941bdf390c32e4e
xarcgit <yasouo@proton.me>
2024-12-29 22:09:38 +0530

    Add Neovim configuration files and plugins for enhanced functionality

---
+++ config/nvim/lua/config/plugins/nvim-cmp.lua
@@ -0,0 +1,110 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+    },
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+    "onsails/lspkind.nvim",
+  --   {
+  --     "zbirenbaum/copilot.lua",
+  --     cmd = "Copilot",
+  --     event = "InsertEnter",
+  --     config = function()
+  --       require("copilot").setup({
+  --         suggestion = { enabled = false },
+  --         panel = { enabled = false },
+  --       })
+  --     end,
+  --   },
+  --   {
+  --     "zbirenbaum/copilot-cmp",
+  --     after = { "copilot.lua" },
+  --     config = function()
+  --       require("copilot_cmp").setup()
+  --     end,
+  --   },
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    local has_words_before = function()
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
+    cmp.setup({
+      completion = { completeopt = "menu,menuone,noselect" },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        -- { name = "copilot" },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+      window = {
+        completion = cmp.config.window.bordered({
+          border = "",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,Search:None",
+          scrollbar = false,
+        }),
+        documentation = cmp.config.window.bordered({
+          border = "",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,Search:None",
+          scrollbar = false,
+        }),
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",  -- VSCode style (icon + text)
+          maxheight = 5,
+          maxwidth = 50,
+          ellipsis_char = "...",
+        }),
+      },
+    })
+  end,
+}
